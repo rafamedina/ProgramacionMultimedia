88d81241a41bb7859c9a6089aade399c423a2801
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-10-30 14:38:54 +0100

    contador contadorito

--- EjemploClaseHilos/app/src/main/java/com/example/ejemploclasehilos/MainActivity.java
+++ EjemploClaseHilos/app/src/main/java/com/example/ejemploclasehilos/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ejemploclasehilos;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +18,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private Button variableQueGuardaUnBoton;
+    Handler handler = new Handler(Looper.getMainLooper());
+
+    TextView viewContador ;
 
 
     @Override
@@ -28,26 +34,51 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         variableQueGuardaUnBoton = findViewById(R.id.buttonGuapardo);
-
+        viewContador = findViewById(R.id.textView_contador);
         variableQueGuardaUnBoton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               new HiloJ().start();
+                new HiloJ().start();
+                new HiloContador().start();
             }
         });
     }
+
     class HiloJ extends Thread {
         @Override
         public void run() {
             try {
                 Thread.sleep(10000);
 
-                runOnUiThread(() ->
+                new Handler(Looper.getMainLooper()).post(() ->
                         Toast.makeText(MainActivity.this, "TERMINE EL SLEEP", Toast.LENGTH_SHORT).show()
                 );
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    class HiloContador extends Thread {
+        @Override
+        public void run() {
+
+
+
+            for (int i = 0; i <= 1500; i++) {
+
+                int finalI = i; // necesario porque i se usa dentro del Runnable
+
+                handler.post(() -> {
+                    viewContador.setText(String.valueOf(finalI));
+                });
+
+                try {
+                    Thread.sleep(1000); // retardo para ver el conteo (10ms)
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
