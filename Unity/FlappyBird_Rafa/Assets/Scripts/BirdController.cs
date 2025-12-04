70ab17843573a4561fac32f7c2114a250e1381c8
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-12-04 14:49:26 +0100

    Add UI and refactor scene for TextMesh Pro support
    Replaced legacy text objects with TextMesh Pro components for 'SCORE' and 'GAME OVER' UI elements. Updated scene objects, materials, and transforms for improved layout and visual clarity. Added necessary TextMesh Pro assets and resources to the project.

--- Unity/FlappyBird_Rafa/Assets/Scripts/BirdController.cs
+++ Unity/FlappyBird_Rafa/Assets/Scripts/BirdController.cs
@@ -1,22 +1,63 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdController : MonoBehaviour
 {
     public float gravityForce;
-    public float jumpForce; 
+    public float jumpForce;
     public Rigidbody rb_body;
+    public int Score = 0;
+    public GameObject pauseMenuGO;
+    public GameObject Scorearriba;
+    private TMP_Text textoScore;
+    private TMP_Text textoScore2;
     void Start()
     {
         rb_body = GetComponent<Rigidbody>();
+        textoScore = Scorearriba.GetComponent<TMP_Text>();
+        textoScore2 = pauseMenuGO.transform.GetChild(3).GetComponent<TMP_Text>();
     }
     void Update()
     {
         rb_body.AddForce(Vector3.down * gravityForce * Time.deltaTime, ForceMode.Force);
-        if (Input.GetKeyDown(KeyCode.Space)==true)
+        if (Input.GetKeyDown(KeyCode.Space) == true)
         {
 
             rb_body.linearVelocity = Vector3.zero;
             rb_body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("He chocado con: " + collision.gameObject.tag);
+        if (collision.gameObject.tag == "Pipe")
+        {
+            Time.timeScale = 0.0f;
+            PauseMenu();
+        }
+    }
+
+    public void PauseMenu()
+    {
+        pauseMenuGO.SetActive(true);
+        Scorearriba.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("He chocado con un trigger" + other.gameObject.name);
+        Score++;
+
+        textoScore.text = Score.ToString();
+        textoScore2.text = Score.ToString();
+
     }
 }
--- Unity/FlappyBird_Rafa/Assets/Scripts/PipeControll.cs
+++ Unity/FlappyBird_Rafa/Assets/Scripts/PipeControll.cs
@@ -3,18 +3,40 @@ using UnityEngine;
 public class PipeControll : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float gravityForce;
-    public Rigidbody rb_body;
+
+    public float speed;
+
+   // public int contador = 0;
+
+    public float xLimit;
+
+    int ultimoIncremento = 0;
     void Start()
     {
-        rb_body = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (transform.position.x < -xLimit)
+        {
+            transform.position = new Vector3(xLimit, Random.Range(3, 17), 0.0f);
+           // contador++;
 
-        rb_body.AddForce(Vector3.left * gravityForce, ForceMode.Force);
+          //  if (contador % 7 == 0 && contador != ultimoIncremento)
+         //   {
+          //      speed += 2;
+          //      ultimoIncremento = contador;
+         //   }
+        }
+
+        transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+
+
+        // rb_body.AddForce(Vector3.left * gravityForce, ForceMode.Force);
     }
+
+
 }
