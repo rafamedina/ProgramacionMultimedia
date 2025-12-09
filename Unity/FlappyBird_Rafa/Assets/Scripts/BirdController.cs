e5de91ccff06c5e0cf1d1173792b5576fdb31572
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-12-09 11:22:32 +0100

    Add sound effects and music to BirdController
    Introduced three AudioSource fields for tap, music, and SFX, and added corresponding audio files to the project. Updated MiEscena.unity to include and reference these audio sources. Modified BirdController to play tap and music sounds on events, handle pause/resume with audio, and play a sound effect on death.

--- Unity/FlappyBird_Rafa/Assets/Scripts/BirdController.cs
+++ Unity/FlappyBird_Rafa/Assets/Scripts/BirdController.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,21 +14,47 @@ public class BirdController : MonoBehaviour
     public GameObject Scorearriba;
     private TMP_Text textoScore;
     private TMP_Text textoScore2;
+
+    private bool pausePanelInterruptor = true;
+    private bool isAlive = true;
+    public AudioSource sFXAudioSource;
+    public AudioSource sFXAudioTap;
+    public AudioSource sFXAudioMusic;
+
     void Start()
     {
+       // sFXAudioSource = GetComponent<AudioSource>();
+     //   sFXAudioSource2 = GetComponent<AudioSource>();
         rb_body = GetComponent<Rigidbody>();
         textoScore = Scorearriba.GetComponent<TMP_Text>();
         textoScore2 = pauseMenuGO.transform.GetChild(3).GetComponent<TMP_Text>();
+        sFXAudioMusic.Play();
+
     }
     void Update()
     {
         rb_body.AddForce(Vector3.down * gravityForce * Time.deltaTime, ForceMode.Force);
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
-
+            sFXAudioTap.Play();
             rb_body.linearVelocity = Vector3.zero;
             rb_body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
         }
+        if (Input.GetKeyDown(KeyCode.Escape) == true && pausePanelInterruptor ==true)
+        {
+            PauseMenu(true);
+            pausePanelInterruptor = false;
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Escape) == true && pausePanelInterruptor == false)
+        {
+            PauseMenu(false);
+            pausePanelInterruptor = true;
+
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -35,20 +62,48 @@ public class BirdController : MonoBehaviour
         //Debug.Log("He chocado con: " + collision.gameObject.tag);
         if (collision.gameObject.tag == "Pipe")
         {
-            Time.timeScale = 0.0f;
-            PauseMenu();
+            PauseDeath();
+
         }
     }
 
-    public void PauseMenu()
+    public void PauseMenu(bool activado)
     {
-        pauseMenuGO.SetActive(true);
-        Scorearriba.SetActive(false);
+        if (activado)
+        {
+            Time.timeScale = 0.0f;
+            pauseMenuGO.SetActive(true);
+            Scorearriba.SetActive(false);
+        }
+        else if (!activado && isAlive)
+        {
+            Time.timeScale = 1.0f;
+            pauseMenuGO.SetActive(false);
+            Scorearriba.SetActive(true);
+        }
+
     }
 
+    public void PauseDeath()
+    {
+        isAlive = false;
+        sFXAudioMusic.Pause();
+   
+            Time.timeScale = 0.0f;
+            pauseMenuGO.SetActive(true);
+            Scorearriba.SetActive(false);
+
+            sFXAudioSource.Play();
+        
+       
+
+    }
     public void Restart()
     {
+
         SceneManager.LoadScene(1);
+        Time.timeScale = 1.0f;
+
     }
 
     private void OnTriggerExit(Collider other)
